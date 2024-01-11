8443bcc07163b70008f99fa5881460905340a5e7
CristhianDavid0207 <emmacris04@gmail.com>
2024-01-10 21:19:58 -0500

    Agregando carpetas y empezando a consumir appi y creando card

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+
+let resultado = fetch("https://memin.io/public/api/v2/users")
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    data.data.forEach(function (element) {
+        let root = document.getElementById("root");
+        let col = document.createElement("div");
+        col.classList.add("col-md-3");
+        root.appendChild(col);
+
+        //CARD
+        let card = document.createElement("div");
+        card.classList.add("card");
+        col.appendChild(card);
+
+        //BODY
+        let card_body = document.createElement("div");
+        card_body.classList.add("card-body");
+        card.appendChild(card_body);
+
+        //PARAFO
+        let p = document.createElement("p");
+        p.innerText = element.name;
+        card_body.appendChild(p);
+    });
+  });
